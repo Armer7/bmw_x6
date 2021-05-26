cbe67b71cd85c0e9a2964da0127c545c0a22b15b
Aleksey Poslushnyak <Poslushnyak@2amigos.us>
2021-05-26 13:21:55 +0300

    First commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+import Accordion from './accordion.js';
+import Burger from './burger.js';
+import Modal from './modal.js';
+import SendForm from './sendForm.js';
+import SmoothScroll from './smothScroll.js';
+import Tabs from './tabs.js';
+
+Accordion();
+Burger();
+Modal();
+SendForm();
+SmoothScroll();
+Tabs();
